ac6df3fef458bc12d8203a0ef06923fd6552d39b
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-01-31 19:25:51 -0200

    Funcional: módulos, riddles, answers, CSS e JS OK

--- js/riddle.js
+++ js/riddle.js
@@ -5,31 +5,25 @@ function submeter()
     console.log('answer: ' + answer);
 
     
-    /* lógica para determinar o número do riddle atual */
+    /* Lógica para determinar o número do riddle atual */
     var currentLevel = window.location.pathname; // absolute path
     currentLevel = currentLevel.substring(currentLevel.lastIndexOf('/')+1);
     currentLevel = currentLevel.slice(0, currentLevel.length - 5);
 
-    /* número do próximo riddle */
+    
+    /* Número do próximo riddle */
     var nextLevel = +currentLevel + 1;
 
-    /* Lógica para obter a resposta do módulo atual */
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.open("GET", "answers.json", false);
-    // xmlHttp.send();
-    // console.log(xmlHttp.responseText);
     
-    var answers1 = {
-	"0": "",
-	"1": ["firefox","mozilla firefox"],
-	"2": ["opensuse","suse","suse linux"],
-	"3": ["donald knuth","knuth"],
-	"4": ["lisp"],
-	"5": ["wep"]
-    };
+    /* Obter a resposta do módulo atual */
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "answers.json", false);
+    xmlHttp.send();
+    console.log(xmlHttp.responseText);
 
+    
     /* Avalia se a resposta do usuário está correta. */
-    var jsonAnswer = answers1;
+    var jsonAnswer = JSON.parse(xmlHttp.responseText);
     var answerCorrect = false;
     
     for (i = 0; i < jsonAnswer[currentLevel].length; ++i) {
